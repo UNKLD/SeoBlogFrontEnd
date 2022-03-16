1ee900ee0368bfc5188df12a9f23bb1539bdd253
Leul <leuldamtew@yahoo.com>
2022-03-16 11:12:44 -0900

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ module.exports = {
   publicRuntimeConfig: {
     APP_NAME: "SEO BLOGS",
     API_DEVELOPMENT: "https://unkld-seo-blog-backend.herokuapp.com/api",
-    PRODUCTION: false,
+    PRODUCTION: true,
     DOMAIN_DEVELOPMENT: "http://localhost:3000",
     DOMAIN_PRODUCTION: "https://anime-blogs.tk",
     DISQUS_SHORTNAME: "anime-seo-2",
