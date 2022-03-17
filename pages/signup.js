2e7a1bd686c5560e932b599b4a1e0f1ceac92fe2
Leul <leuldamtew@yahoo.com>
2022-03-17 08:34:28 -0900

    Update signup.js

--- pages/signup.js
+++ pages/signup.js
@@ -4,10 +4,12 @@ import SignupComponent from '../components/auth/SignupComponent';
 function signup() {
   return (
     <Layout>
-      <h2 className='text-center pt-4 pb-4'>Signup</h2>
-      <div className='row'>
-        <div className='col-md-6 offset-md-3'>
-          <SignupComponent />
+      <div className="container-fluid">
+        <h2 className="text-center pt-4 pb-4">Signup</h2>
+        <div className="row">
+          <div className="col-md-6 offset-md-3">
+            <SignupComponent />
+          </div>
         </div>
       </div>
     </Layout>
