6a73679fd19fbd6840eed647dde21cf40532b09c
Leul <leuldamtew@yahoo.com>
2023-09-02 07:12:52 -0900

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ import getConfig from "next/config";
 const { publicRuntimeConfig } = getConfig();
 
 export const API = publicRuntimeConfig.PRODUCTION
-  ? "https://unkld-seo-blog-backend.herokuapp.com/api"
+  ? "https://seoblogbackend-leuldamtew.b4a.run/api"
   : publicRuntimeConfig.API_DEVELOPMENT;
 
 export const APP_NAME = publicRuntimeConfig.APP_NAME;
